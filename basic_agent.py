a60532ea3d6a365bd749b1acf9f002ff8b3c2eeb
Hassaan Saeed <48230146+hassaansaeed84@users.noreply.github.com>
2021-05-05 10:43:19 -0400

    Basic_agent

---
+++ basic_agent.py
@@ -0,0 +1,14 @@
+import cv2
+import math
+from random import randint
+import numpy as np
+import copy
+
+def basic_agent():
+    image = cv2.imread('beach.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    middle = int(len(image[0]) / 2)
+    left_half = image[:, :middle]
+    right_half = image[:, middle:]
+
